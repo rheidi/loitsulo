01079a69d276d90a0517868c64f9f75d5b996a6b
Heidi Rajala <heidi.rajala@integrify.io>
2023-10-04 09:40:34 +0300

    Move spell card to its own component

---
+++ spellbook/src/components/SpellCard.tsx
@@ -0,0 +1,97 @@
+import { Card, CardHeader, IconButton, CardContent, Typography, CardActions, Collapse, IconButtonProps, styled } from "@mui/material"
+import CloseIcon from '@mui/icons-material/Close'
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Spell } from "../types/Spell"
+import { useContext } from "react";
+import SpellContext from "./SpellContext";
+import React from "react";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}))
+
+const SpellCard = ({spell}: {spell: Spell}) => {
+  const { selectedSpells, setSelectedSpells } = useContext(SpellContext)
+
+  const handleRemove = (s: Spell) => () => {
+    setSelectedSpells((selectedSpells) => {
+      return selectedSpells.filter((spell) => spell.slug !== s.slug)
+    })
+  }
+  
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card>
+      <CardHeader
+        title={spell.name}
+        action={
+          <IconButton role='remove' onClick={handleRemove(spell)} aria-label="remove spell">
+            <CloseIcon />
+          </IconButton>
+        }
+      />
+      <CardContent>
+        <Typography sx={{ mb: 1.2 }} color="text.secondary">{spell.level} {spell.school.toLowerCase()}</Typography>
+        <Typography variant="subtitle2">
+          Casting time: {spell.casting_time}<br />
+          Range: {spell.range}<br />
+          Components: {spell.components}<br />
+          {(spell.requires_material_components) ?
+            (
+              <Typography variant="subtitle2">Materials: {spell.material}<br /></Typography>
+            ) : (
+              <></>
+            )}
+          Duration: {spell.duration}<br />
+          
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {(spell.requires_concentration) ?
+          (
+            <CopyrightIcon />
+          ) : (
+            <></>
+          )}
+        {(spell.can_be_cast_as_ritual) ?
+          (
+            <Typography variant="subtitle2">®</Typography>
+          ) : (
+            <></>
+          )}
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{spell.desc}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  )
+}
+
+export default SpellCard
--- spellbook/src/pages/MySpells.tsx
+++ spellbook/src/pages/MySpells.tsx
@@ -1,34 +1,12 @@
 import { Box, Button, Card, CardActions, CardContent, CardHeader, Collapse, Grid, IconButton, IconButtonProps, Link, Popper, Typography, styled } from '@mui/material'
-import CloseIcon from '@mui/icons-material/Close'
-import CopyrightIcon from '@mui/icons-material/Copyright';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import React, { useContext } from 'react'
 import SpellContext from '../components/SpellContext'
 import { Spell } from '../types/Spell'
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import SpellCard from '../components/SpellCard'
 
 const MySpells = () => {
   const { selectedSpells, setSelectedSpells } = useContext(SpellContext)
-
-  const handleRemove = (s: Spell) => () => {
-    setSelectedSpells((selectedSpells) => {
-      return selectedSpells.filter((spell) => spell.slug !== s.slug)
-    })
-  }
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
@@ -38,12 +16,6 @@ const MySpells = () => {
 
   const open = Boolean(anchorEl)
   const id = open ? 'simple-popper' : undefined
-
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Box sx={{ p: 2 }}>
@@ -67,59 +39,7 @@ const MySpells = () => {
           <Grid container spacing={2} sx={{pt:1}}>
             {selectedSpells.map((s) => (
               <Grid item key={s.slug} width={300}>
-                <Card>
-                  <CardHeader
-                    title={s.name}
-                    action={
-                      <IconButton role='remove' onClick={handleRemove(s)} aria-label="remove spell">
-                        <CloseIcon />
-                      </IconButton>
-                    }
-                  />
-                  <CardContent>
-                    <Typography sx={{ mb: 1.2 }} color="text.secondary">{s.level} {s.school.toLowerCase()}</Typography>
-                    <Typography variant="subtitle2">
-                      Casting time: {s.casting_time}<br />
-                      Range: {s.range}<br />
-                      Components: {s.components}<br />
-                      {(s.requires_material_components) ?
-                        (
-                          <Typography variant="subtitle2">Materials: {s.material}<br /></Typography>
-                        ) : (
-                          <></>
-                        )}
-                      Duration: {s.duration}<br />
-                      
-                    </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    {(s.requires_concentration) ?
-                      (
-                        <CopyrightIcon />
-                      ) : (
-                        <></>
-                      )}
-                    {(s.can_be_cast_as_ritual) ?
-                      (
-                        <Typography variant="subtitle2">R</Typography>
-                      ) : (
-                        <></>
-                      )}
-                    <ExpandMore
-                      expand={expanded}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <Typography paragraph>{s.desc}</Typography>
-                    </CardContent>
-                  </Collapse>
-                </Card>
+                <SpellCard spell={s}></SpellCard>
               </Grid>
             ))}
           </Grid>
